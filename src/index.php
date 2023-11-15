139d2e0ae4f60174a655d005bf72382dde3ecec8
Dyl <dylanteozxc@gmail.com>
2023-11-15 13:30:44 +0800

    .

--- src/index.php
+++ src/index.php
@@ -1,46 +1,19 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="UTF-8">
-    <title>Test</title>
+    <title>Your Website</title>
+    <!-- Add any additional head elements here -->
 </head>
 <body>
-    <form action="search.php" method="post">
-        <label for="search">Search:</label>
-        <input type="text" id="search" name="search" placeholder="Enter your search term">
-        <button type="submit">Search</button>
-    </form>
 
-    <?php
-    if (isset($_POST['search'])) {
-        $search = $_POST['search'];
-        
-        // Sanitize the input (strip_tags removes any HTML/PHP tags)
-        $search = strip_tags($search);
-        
-        // Connect to your database using PDO
-        $dsn = 'mysql:host=localhost;dbname=your_database_name;charset=utf8mb4';
-        $username = 'your_username';
-        $password = 'your_password';
-        
-        try {
-            $pdo = new PDO($dsn, $username, $password);
-            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            
-            // Use a prepared statement to execute the query
-            $stmt = $pdo->prepare("SELECT * FROM your_table_name WHERE column_name LIKE :search");
-            $stmt->bindValue(':search', '%' . $search . '%', PDO::PARAM_STR);
-            $stmt->execute();
-            
-            // Process and display the results
-            while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-                // Display the results here
-                echo htmlspecialchars($row['column_name']) . '<br>';
-            }
-        } catch (PDOException $e) {
-            echo 'Error: ' . $e->getMessage();
-        }
-    }
-    ?>
+<h2>Search Bar</h2>
+
+<form action="search.php" method="get">
+    <input type="text" name="query" placeholder="Search...">
+    <input type="submit" value="Search">
+</form>
+
+<!-- Add the rest of your page content here -->
+
 </body>
 </html>
--- src/search.php
+++ src/search.php
@@ -1,50 +1,39 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <title>Search Results</title>
-</head>
-<body>
-    <h1>Search Results</h1>
+<?php
+// Include your database connection here
 
-    <?php
-    if (isset($_POST['search'])) {
-        $search = $_POST['search'];
+function sanitize_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
 
-        // Sanitize the input (strip_tags removes any HTML/PHP tags)
-        $search = strip_tags($search);
-
-        // Connect to your database using PDO
-        $dsn = 'mysql:host=localhost;dbname=your_database_name;charset=utf8mb4';
-        $username = 'your_username';
-        $password = 'your_password';
-
-        try {
-            $pdo = new PDO($dsn, $username, $password);
-            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-            // Use a prepared statement to execute the query
-            $stmt = $pdo->prepare("SELECT * FROM your_table_name WHERE column_name LIKE :search");
-            $stmt->bindValue(':search', '%' . $search . '%', PDO::PARAM_STR);
-            $stmt->execute();
-
-            // Display the search results
-            echo '<ul>';
-            while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-                echo '<li>' . htmlspecialchars($row['column_name']) . '</li>';
-            }
-            echo '</ul>';
-            
-            // Add a button to return to the homepage
-            echo '<a href="index.php">Back to Homepage</a>';
-        } catch (PDOException $e) {
-            echo 'Error: ' . $e->getMessage();
+function isSQLInjection($input) {
+    $sql_keywords = ["SELECT", "INSERT", "UPDATE", "DELETE", "DROP", "UNION", "WHERE", "OR", "AND"];
+    foreach ($sql_keywords as $keyword) {
+        if (stripos($input, $keyword) !== false) {
+            return true;
         }
-    } else {
-        // If no search term is provided, display a message
-        echo '<p>No search term provided.</p>';
-        echo '<a href="index.php">Back to Homepage</a>';
     }
-    ?>
-</body>
-</html>
+    return false;
+}
+
+// Check if the query parameter is set
+if (isset($_GET['query'])) {
+    $query = sanitize_input($_GET['query']);
+
+    // Check for SQL injection
+    if (isSQLInjection($query)) {
+        echo "Invalid search query.";
+    } else {
+        // Perform your database query here
+        // For example: SELECT * FROM your_table WHERE your_column LIKE '%$query%'
+
+        // Fetch and display the results
+        // Example: while ($row = mysqli_fetch_assoc($result)) { echo $row['your_column']; }
+    }
+} else {
+    echo "No search query provided.";
+}
+
+?>
